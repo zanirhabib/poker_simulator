787242a8bc5426b79e4fd2f7907a4cf4bf49dd02
Zanir Habib <zanir.habib@gmail.com>
2019-12-25 22:45:52 -0600

    Working on python version - made basic outline

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # poker_simulator
 This is just a project to calculate the different probabilities and occurences of different poker hands
+
+This is based on Texas Hold 'Em. (Each player gets two cards, best hand wins)
+
+Assumptions: 
+ - No Folding (Feature to be implemented)
+ - The Flop, River, and Turn are dealt after each player receives their cards
+
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 #function to 
 
+class hand:
+    firstCard = 'B'
+    secondCard = 'B'
+
+
 def pass_cards(numppl):
+
+#could possibly implement a hash table to 
+#make sure player doesn't get the same card multiple times
+
+#create a class for each player
+
+#each player
+
 
 #have to make sure that card is unique
 #can make a random number generator
+
+#going to make a list of 
+
+
+
+
