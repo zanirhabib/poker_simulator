64cf46f0f3a87276e16cf251f9d5a46a25c8ce71
Zanir Habib <zanir.habib@gmail.com>
2020-01-13 21:15:43 -0800

    Working with arrays

--- cpp_src/main.cpp
+++ cpp_src/main.cpp
@@ -1,16 +1,50 @@
-char * distribute_hands(numHands)
+#include<stdio.h>
+#include<iostream>
+#include <cstdlib>
+
+using namespace std;
+
+char * distribute_hands(int numHands)
 {
 
     //return array of pointers to arrays of hands
+    for(int i = 0; i < numHands; i++)
+    {
+    	for(int i = 0; i < 3; i++)
+	{
+	    //check value, if value is good, pop to array
+	}
+	//set a random number between 1 and 13 (represents each of the cards)
+         
+	//pair with a suit
+	//check if it already exists
+   	
+
+	
+
+
+    }	
+    char test1[3] = {'a','b','c'};
+    char* ptr;
+    ptr = test1;
+    char test2[3] = {'d','e','f'};
 
     //eac
 
 }
 
+
 int main()
 {
 
-	//distribute cards
-	distribute_hands(numHands);
-
+    //distribute cards
+    int numHands;
+    numHands = 0;
+    scanf("%d", &numHands);
+    //distribute_hands(numHands);
+    int ra[3] = {2,1};
+    cout << *ra << endl;
 }
+
+
+
